3dbc52eedfbd061120c11afd6fb7d1b4c7480696
Diana Spivak <d0990683511@gmail.com>
2022-08-22 17:24:10 +0300

    Added log out functionality

--- src/components/ChatListView/components/AccountInfoBlock/AccountInfoBlock.jsx
+++ src/components/ChatListView/components/AccountInfoBlock/AccountInfoBlock.jsx
@@ -1,11 +1,23 @@
 import "./AccountInfoBlock.scss";
 import AccountPhoto from "../../../AccountPhoto/AccountPhoto";
+import { SignOut } from "phosphor-react";
+import { useNavigate } from "react-router-dom";
 
 function AccountInfoBlock() {
+  const navigate = useNavigate();
   return (
     <div className="account-info-block">
       <div className="account-info-block__acc-photo">
         <AccountPhoto online={true} />
+      </div>
+      <div
+        className="account-info-block__log-out"
+        onClick={() => {
+          localStorage.removeItem("loggedIn");
+          navigate("/");
+        }}
+      >
+        <SignOut className="account-info-block__log-out-icon" />
       </div>
     </div>
   );
